20a852ae77a62a0d023c84c4081a27c3e484e051
Saurabh <skkashyap2328@gmail.com>
2024-02-06 13:42:56 +0530

    HELLO

---
+++ script.js
@@ -0,0 +1,24 @@
+// shopping cart script //
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+    shoppingCart.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
+// login form script //
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+    loginForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+
+}
+
+window.onscroll = () =>
+{
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
